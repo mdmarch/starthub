5cfabc82837e8b40ac5d565db548137fe1d89aca
mdmarch <64365209+mdmarch@users.noreply.github.com>
2023-11-30 17:33:40 +0300

    initital commit

---
+++ src/main/java/ru/hse/starthub/StarthubApplication.java
@@ -0,0 +1,13 @@
+package ru.hse.starthub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarthubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarthubApplication.class, args);
+	}
+
+}
